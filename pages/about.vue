09e6f1622645d2feeb5fe52b50ba4986d6e00532
Daniel Roe <daniel@roe.dev>
2020-09-13 12:23:12 +0100

    chore: init

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>{{ $route.name }}</p>
+
+    <p>{{ $nuxt.context.route.name }}</p>
+
+    <NuxtChild />
+  </div>
+</template>
+<script>
+export default {
+  created() {
+    if (process.server) {
+      const { route, req } = this.$nuxt.context;
+      const ctx1 = this.$nuxt.context;
+
+      setTimeout(() => {
+        console.log(
+          "--------",
+          this.$route.fullPath,
+          this.$nuxt.context.route.fullPath,
+          this.$nuxt.context == ctx1
+        );
+      }, 1000);
+    }
+  },
+};
+</script>
